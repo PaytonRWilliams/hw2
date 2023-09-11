5da53655d59fea136d3278161876bba3fbce3cfa
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-09-11 14:15:53 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -13,26 +13,18 @@ include "view-footer.php";
 
 
 function getDisplay(){
-if (isset($_POST['my-name'])){
+if (isset($_POST['my-name']))
+{
 
 
 return "<p>The value sent is:</p>" .$_POST['my-name'];
 
-
-
-}else{
+}
+else{
 return "<p>Nothing posted to the page.</p>";
 
-  
 
-
-}
-
-
-
-
-
-  
+} 
 }
 
 ?>
