0142d811d55f46d451b30a5c9e90716be35bb47e
PaytonRWilliams <142914907+PaytonRWilliams@users.noreply.github.com>
2023-09-11 10:37:10 -0500

    Create post.php

---
+++ post.php
@@ -0,0 +1,15 @@
+<?php
+
+$pageTitle="Home";
+include "view-header.php";
+?>
+<h1>Post</h1>h1>
+<form method="post action="post-result.php">
+  <input type="text" name="my-name">
+  <input type="submit" value="Post data">
+    
+</form>
+
+<?php
+include "view-footer.php";
+?>
